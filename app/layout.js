98d5476255eee28e9dc7ebd8282191adb2173014
Kevin Diaz <kevin.diaz@alumnos.uv.cl>
2022-11-22 11:04:01 -0300

    primer layout

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,15 @@
 export default function RootLayout ({ children }) {
   return (
     <html>
-      <head />
-      <body>{children}</body>
+      <head>Esto es un titulo</head>
+      <body>
+        <nav>
+          <ul>
+            <li>Home</li>
+          </ul>
+        </nav>
+        {children}
+      </body>
     </html>
   )
 }
